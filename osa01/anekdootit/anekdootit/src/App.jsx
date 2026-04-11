c5d12cbd7296cfa3373d67ba987587e25a9e87af
mrmirror5 <mirstumiro@gmail.com>
2026-04-11 17:12:43 +0300

    Osa01 tehty

---
+++ osa01/anekdootit/anekdootit/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const AnectodeOfDay = ({ anecdote, votes, onVote, onNext }) => {
+  return (
+    <>
+      <h2>Anecdote of the day</h2>
+      <div>{anecdote}</div>
+      <div>has {votes} votes</div>
+      <button onClick={onVote}>vote</button>
+      <button onClick={onNext}>next anecdote</button>
+    </>
+  );
+};
+
+const AnectodeWithMost = ({ mostPopularAnectode, mostPopularVotes }) => {
+  return (
+    <>
+      <h2>Anecdote with most votes</h2>
+      <div>{mostPopularAnectode}</div>
+      <div>has {mostPopularVotes} votes</div>
+    </>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const handleRandomQuote = () => {
+    const randInt = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randInt);
+  };
+
+  const handleVote = () => {
+    setVotes((votes) => {
+      const copy = [...votes];
+      copy[selected] += 1;
+      return copy;
+    });
+  };
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const findMostPopular = () => {
+    const index = votes.indexOf(Math.max(...votes));
+    return anecdotes[index];
+  };
+
+  return (
+    <>
+      <AnectodeOfDay
+        anecdote={anecdotes[selected]}
+        votes={votes[selected]}
+        onVote={handleVote}
+        onNext={handleRandomQuote}
+      ></AnectodeOfDay>
+      <AnectodeWithMost
+        mostPopularAnectode={findMostPopular()}
+        mostPopularVotes={Math.max(...votes)}
+      ></AnectodeWithMost>
+    </>
+  );
+};
+
+export default App;
